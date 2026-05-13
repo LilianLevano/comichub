1096441e5f8befbc7c7e9727904cd3d6eca6786c
Lilian Levano <zey.lilianlevano@gmail.com>
2026-05-13 17:01:49 +0200

    component "back button" gemaakt

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -13,11 +13,7 @@
             </ul>
         </div>
 
-        <a href="/categories"
-           class="inline-flex items-center gap-2 mt-10 px-5 py-2.5 bg-blue-500 hover:bg-blue-600 active:scale-95 text-white text-sm font-medium rounded-lg transition-all duration-150"
-        >
-            ← Back to all categories
-        </a>
+        <x-back-button/>
     </div>
 
 </x-site-layout>
---
+++ resources/views/components/back-button.blade.php
@@ -0,0 +1,6 @@
+<button
+    onclick="history.back()"
+    class="inline-flex items-center gap-2 mt-10 px-5 py-2.5 hover:cursor-pointer bg-blue-500 hover:bg-blue-600 active:scale-95 text-white text-sm font-medium rounded-lg transition-all duration-150"
+>
+    ← Back
+</button>
