6c38d7b35014efe3f865fdaa01030c9378c9d6e5
Lilian Levano <zey.lilianlevano@gmail.com>
2026-05-13 17:13:02 +0200

    voeg een link naar de show van de comic in welcome pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,10 @@
             <h1  class="text-2xl font-bold tracking-tight text-gray-800 border-b-4 border-blue-400 pb-2" >Latest Publication</h1>
 
             <div class="flex flex-col gap-8 w-[90%] max-w-xl mx-auto mt-8">
-                    <div class="bg-white rounded-2xl shadow-md border border-black/10 p-6 flex flex-col items-center gap-6">
-                        <img src="{{ $comic->image_path }}" alt="{{ $comic->title }}" class="w-32 h-24 object-cover rounded-xl">
+                    <div class="bg-white rounded-2xl shadow-md border border-black/10 p-6 flex flex-col items-center gap-6 group">
+                        <img src="{{ $comic->image_path }}" alt="{{ $comic->title }}" class="w-50 h-64 object-cover rounded-xl">
                         <div class="flex flex-col gap-2 flex-1">
-                            <h2 class="text-xl font-bold">{{ $comic->title }}</h2>
+                            <h2 class="text-xl font-bold group-hover:cursor-pointer group-hover:text-blue-400 transition-all duration-300"><a href="/comics/{{$comic->id}}">{{ $comic->title }}</a> </h2>
                             <p class="text-gray-500 text-sm">{{ $comic->description }}</p>
                             <div class="flex gap-4 text-sm text-gray-400">
                                 <span>✍️ {{ $comic->author }}</span>
