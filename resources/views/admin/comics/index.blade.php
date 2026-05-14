6133f04afe9a58f473e5f521879ede53d611c95a
Lilian Levano <zey.lilianlevano@gmail.com>
2026-05-14 11:42:43 +0200

    STYLE: changed cards style to adapt to bigger text

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -17,7 +17,7 @@
                 <img src="{{ $comic->image_path }}" alt="{{ $comic->title }}" class="w-64 h-80 object-cover rounded-xl">
                 <div class="flex flex-col gap-2 flex-1">
                     <h2 class="text-xl font-bold group-hover:cursor-pointer group-hover:text-blue-400 transition-all duration-300"><a href="/comics/{{$comic->id}}">{{ $comic->title }}</a> </h2>
-                    <p class="text-gray-500 text-sm">{{ $comic->description }}</p>
+                    <p class="text-gray-500 text-sm break-words max-w-2xl">{{ $comic->description }}</p>
                     <div class="flex gap-4 text-sm text-gray-400">
                         <span>✍️ {{ $comic->author }}</span>
                         <span> <a href="/categories/{{$comic->category->id}}" class="hover:text-blue-400 underline">📁 {{ $comic->category->name }}</a>  </span>
